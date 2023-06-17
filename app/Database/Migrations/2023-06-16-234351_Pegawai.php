85f9e2ad2ed3226b083076d9112fb6ab194a05a1
Luthvi Hari Fitriadi <59356454+luthvihari@users.noreply.github.com>
2023-06-17 07:37:32 +0700

    setting model dan migration

---
+++ app/Database/Migrations/2023-06-16-234351_Pegawai.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class Pegawai extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'id' => [
+                'type'           => 'INT',
+                'constraint'     => 5,
+                'unsigned'       => true,
+                'auto_increment' => true,
+            ],
+            'nama' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'jabatan' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'bidang' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'alamat' => [
+                'type' => 'TEXT',
+                'null' => true,
+            ],
+            'email' => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'created_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            'updated_at' => [
+                'type' => 'DATETIME',
+                'null' => true,
+            ],
+            
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('pegawai');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('pegawai');
+    }
+}
--- app/Models/Pegawai.php
+++ app/Models/Pegawai.php
@@ -6,36 +6,16 @@ use CodeIgniter\Model;
 
 class Pegawai extends Model
 {
-    protected $DBGroup          = 'default';
-    protected $table            = 'pegawais';
+   
+    protected $table            = 'pegawai';
     protected $primaryKey       = 'id';
     protected $useAutoIncrement = true;
-    protected $returnType       = 'array';
-    protected $useSoftDeletes   = false;
-    protected $protectFields    = true;
-    protected $allowedFields    = [];
+    protected $allowedFields    = ['nama', 'jabatan', 'bidang', 'alamat', 'email'];
 
     // Dates
-    protected $useTimestamps = false;
+    protected $useTimestamps = true;
     protected $dateFormat    = 'datetime';
     protected $createdField  = 'created_at';
     protected $updatedField  = 'updated_at';
-    protected $deletedField  = 'deleted_at';
-
-    // Validation
-    protected $validationRules      = [];
-    protected $validationMessages   = [];
-    protected $skipValidation       = false;
-    protected $cleanValidationRules = true;
-
-    // Callbacks
-    protected $allowCallbacks = true;
-    protected $beforeInsert   = [];
-    protected $afterInsert    = [];
-    protected $beforeUpdate   = [];
-    protected $afterUpdate    = [];
-    protected $beforeFind     = [];
-    protected $afterFind      = [];
-    protected $beforeDelete   = [];
-    protected $afterDelete    = [];
+   
 }
